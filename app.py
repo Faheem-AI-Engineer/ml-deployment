879057c29df653a411f88db507f3a1e0566ddc12
Faheem-AI-Engineer <muhammadfaheemf18@gmail.com>
2025-03-06 17:13:28 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from config import *
 from retriever import initialize_pinecone, create_hybrid_retriever
 from document_processor import convert_pdf_to_markdown, process_and_chunk_document
 from llm_chain import initialize_llm, create_retrieval_chain
-import nltk
-nltk.download('punkt_tab')
+from streamlit import config as _config
+_config.set_option("global.wideMode", True)
 
 # Initialize torch settings
 torch.classes.__path__ = []
